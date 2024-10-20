ec8860dd2f630d9d6d81c0bf81f576a2d27ff630
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2024-10-20 04:07:45 +0300

    SwitchCaseDemo
    This Java project demonstrates the use of the `switch` case structure, which allows you to select one of many code blocks to be executed based on the value of a variable. The project covers basic switch case usage and compares it to `if-else` for readability and efficiency in certain scenarios.

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan gün numarası alma
+        System.out.print("Bir gün numarası girin (1-7): ");
+        int day = scanner.nextInt();
+
+        // Switch case yapısı ile gün belirleme
+        switch (day) {
+            case 1:
+                System.out.println("Pazartesi");
+                break;
+            case 2:
+                System.out.println("Salı");
+                break;
+            case 3:
+                System.out.println("Çarşamba");
+                break;
+            case 4:
+                System.out.println("Perşembe");
+                break;
+            case 5:
+                System.out.println("Cuma");
+                break;
+            case 6:
+                System.out.println("Cumartesi");
+                break;
+            case 7:
+                System.out.println("Pazar");
+                break;
+            default:
+                System.out.println("Geçersiz gün numarası!");
+        }
+
+        // Başka bir örnek - aylar
+        System.out.print("Bir ay numarası girin (1-12): ");
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 1:
+                System.out.println("Ocak");
+                break;
+            case 2:
+                System.out.println("Şubat");
+                break;
+            case 3:
+                System.out.println("Mart");
+                break;
+            case 4:
+                System.out.println("Nisan");
+                break;
+            case 5:
+                System.out.println("Mayıs");
+                break;
+            case 6:
+                System.out.println("Haziran");
+                break;
+            case 7:
+                System.out.println("Temmuz");
+                break;
+            case 8:
+                System.out.println("Ağustos");
+                break;
+            case 9:
+                System.out.println("Eylül");
+                break;
+            case 10:
+                System.out.println("Ekim");
+                break;
+            case 11:
+                System.out.println("Kasım");
+                break;
+            case 12:
+                System.out.println("Aralık");
+                break;
+            default:
+                System.out.println("Geçersiz ay numarası!");
+        }
+
+        // Scanner'ı kapatma
+        scanner.close();
+    }
+}
